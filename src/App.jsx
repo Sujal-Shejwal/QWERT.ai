e940c27c4b7997ca824b658e87a694a8cbb59178
Sujal-Shejwal <sujalshejwal03@gmail.com>
2026-06-18 19:59:00 +0530

    Added navbar and updated assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 
-import Home from './pages/Home';
-import Layout from './pages/Layout';
-import Dashboard from './pages/Dashboard';
-import WriteArtical from './pages/WriteArtical';
-import BlogTitles from './pages/BlogTitles';
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import WriteArtical from './pages/WriteArtical'
+import BlogTitles from './pages/BlogTitles'
+
+import GenerateImg from './pages/Generateimg'
+import RemoveBackground from './pages/RemoveBackground'
+import RemoveObject from './pages/RemoveObject'
+import ReviewResume from './pages/ReviewResume'
+import Community from './pages/Community'
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
 
-      <Route path="/ai" element={<Layout />}>
-        <Route index element={<Dashboard />} />
-        <Route path="write-artical" element={<WriteArtical />} />
-        <Route path="blog-titles" element={<BlogTitles />} />
-      </Route>
-    </Routes>
-  );
-};
+        <Route path='ai' element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path='write-artical' element={<WriteArtical />} />
+          <Route path='blog-titles' element={<BlogTitles />} />
+          <Route path='generate-img' element={<GenerateImg />} />
+          <Route path='remove-background' element={<RemoveBackground />} />
+          <Route path='remove-object' element={<RemoveObject />} />
+          <Route path='review-resume' element={<ReviewResume />} />
+          <Route path='community' element={<Community />} />
+        </Route>
+      </Routes>
+    </div>
+  )
+}
 
-export default App;
+export default App
