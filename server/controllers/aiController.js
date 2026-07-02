fee4739ba195a97434d46ba89090dc034e1156af
Sujal-Shejwal <sujalshejwal03@gmail.com>
2026-07-02 17:34:50 +0530

    Added AI backend setup

---
+++ server/controllers/aiController.js
@@ -0,0 +1,55 @@
+import OpenAI from "OpenAI";
+import sql from "../configs/db.js";
+import { clerkClient } from "@clerk/express";
+
+const AI = new OpenAI(
+    api_key=process.env.GEMINI_API_KEY,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+
+export const generateArticle = async (req, res)=> {
+    try {
+        const { userId } = req.auth();
+        const { prompt, length } = req.body;
+        const plan = req.plan;
+        const free_usage = req.free_usage;
+
+        if(plan !== 'premium' && free_usage >= 10) {
+            return res.json({success: false, message: "Free usage limit reached. Please upgrade to premium plan."})
+        }
+
+
+    const response = await AI.chat.completions.create(
+    model="gemini-3.5-flash",
+    messages=[
+
+        {
+            role: "user",
+            content: prompt,
+        }
+    ]
+    temperature=0.7,
+    max_tokens=length
+)
+
+
+   const content = response.choices[0].message.content
+    
+   await sql`INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, ${prompt}, ${content}, 'article')`;
+
+   if (plan !=== 'premium') {
+    await clerkClient.users.updateUserMetadata(userId, {
+        privateMetadata: {
+            free_usage: free_usage + 1
+        }
+    })
+   }
+
+   res.json({success : true, content})
+
+    } catch (error) {
+        consol.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
